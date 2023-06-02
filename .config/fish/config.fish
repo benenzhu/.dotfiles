97a091fae72c30399e0748665b72f00e4db4f0ef
朱陶宇（实习） <zhutaoyu@xiaohongshu.com>
2023-06-02 03:30:52 +0000

    添加开发机上的配置到git准备进行比较

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+abbr -a vi vim 
+abbr -a nvim nvim 
+abbr -a vim vim 
+abbr -a c clear
+export https_proxy=10.7.4.2:3128
---
+++ .config/nvim/lua/plugins/vscode.lua
@@ -0,0 +1,12 @@
+-- if true then return {} end
+
+return {
+  {
+    "Mofiqul/vscode.nvim",
+    config = function()
+      -- Fold options
+      vim.o.background = "dark"
+      require("ufo").setup()
+    end,
+  },
+}
