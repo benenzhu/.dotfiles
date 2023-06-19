2a6f1b36b1958e21d8d449622b5883fc023c83b9
朱陶宇（实习） <zhutaoyu@xiaohongshu.com>
2023-06-19 16:19:01 +0800

    更新change log 0.1.35

--- .config/nvim/lua/plugins/autosave.lua
+++ .config/nvim/lua/plugins/autosave.lua
@@ -7,5 +7,18 @@ return {
         -- or just leave it empty :)
       })
     end,
+
+    {
+      "crusj/bookmarks.nvim",
+      keys = {
+        { "<tab><tab>", mode = { "n" } },
+      },
+      branch = "main",
+      dependencies = { "nvim-web-devicons" },
+      config = function()
+        require("bookmarks").setup()
+        require("telescope").load_extension("bookmarks")
+      end,
+    },
   },
 }
